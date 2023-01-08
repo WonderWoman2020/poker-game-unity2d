4369847349dc3ba88e23435b3b081469b78682ad
WonderWoman <74731481+WonderWoman2020@users.noreply.github.com>
2023-01-08 05:28:12 +0100

    Added cards collection class

--- StandaloneClassesC#/PokerGameClasses/PokerGameClasses/Program.cs
+++ StandaloneClassesC#/PokerGameClasses/PokerGameClasses/Program.cs
@@ -6,11 +6,27 @@ namespace PokerGameClasses
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-
             Card sampleCard = new Card(CardSign.Heart, CardValue.Ten);
             Console.WriteLine(sampleCard.Name);
             Console.WriteLine(sampleCard.CardColorToString());
+
+            Console.WriteLine();
+
+            CardsCollection sampleDeck = CardsCollection.CreateStandardDeck();
+            sampleDeck.Cards.ForEach(c => Console.WriteLine(c.Name));
+            Console.WriteLine();
+            Card takenCard = sampleDeck.TakeOutCard(CardSign.Heart, CardValue.Ace);
+            sampleDeck.Cards.ForEach(c => Console.WriteLine(c.Name));
+            Console.WriteLine();
+            Console.WriteLine(takenCard.Name);
+            Console.WriteLine(sampleDeck.TakeOutCard(CardSign.Heart, CardValue.Ace));
+            sampleDeck.Cards.ForEach(c => Console.WriteLine(c.Name));
+
+            Console.WriteLine();
+
+            sampleDeck.ShuffleCards();
+            sampleDeck.Cards.ForEach(c => Console.WriteLine(c.Name));
+            
         }
     }
 }
