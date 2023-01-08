85b4a29dd7186bafb9ccc4d92f2a4be6b9c3f059
WonderWoman <74731481+WonderWoman2020@users.noreply.github.com>
2023-01-08 04:38:17 +0100

    Added card class

--- StandaloneClassesC#/PokerGameClasses/PokerGameClasses/Program.cs
+++ StandaloneClassesC#/PokerGameClasses/PokerGameClasses/Program.cs
@@ -7,6 +7,10 @@ namespace PokerGameClasses
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Card sampleCard = new Card(CardSign.Heart, CardValue.Ten);
+            Console.WriteLine(sampleCard.Name);
+            Console.WriteLine(sampleCard.CardColorToString());
         }
     }
 }
