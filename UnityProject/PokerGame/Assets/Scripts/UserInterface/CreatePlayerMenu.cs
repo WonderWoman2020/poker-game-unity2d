70a1fa8a0e7cf6aa40273bd566b7ff32d1b73be4
WonderWoman <74731481+WonderWoman2020@users.noreply.github.com>
2023-01-17 22:45:36 +0100

    Added input getting in CreatePlayer and CreateTable (without radio buttons for now)

--- UnityProject/PokerGame/Assets/Scripts/UserInterface/CreatePlayerMenu.cs
+++ UnityProject/PokerGame/Assets/Scripts/UserInterface/CreatePlayerMenu.cs
@@ -6,10 +6,16 @@ using UnityEngine.SceneManagement;
 
 using PokerGameClasses;
 
+using System;
+
 public class CreatePlayerMenu : MonoBehaviour
 {
     [SerializeField] private Button createButton;
     [SerializeField] private Button backToMenuButton;
+
+    private string playerNick;
+    private string chips;
+    private string xp;
 
     public void OnBackToMenuButton()
     {
@@ -18,15 +24,52 @@ public class CreatePlayerMenu : MonoBehaviour
     public void OnCreateButton()
     {
         Player player = new HumanPlayer("I'm main player", PlayerType.Human);
+        this.SetPlayerInputData(player);
         MyGameManager.Instance.AddPlayerToGame(player);
         Debug.Log("Created player "+player.Nick);
         //SceneManager.LoadScene("Table");
         SceneManager.LoadScene("PlayMenu");
     }
+
+    public void ReadPlayerNick(string nick)
+    {
+        this.playerNick = nick;
+        Debug.Log(this.playerNick);
+    }
+
+    public void ReadChips(string chips)
+    {
+        this.chips = chips;
+        Debug.Log(this.chips);
+    }
+
+    public void ReadXP(string xp)
+    {
+        this.xp = xp;
+        Debug.Log(this.xp);
+    }
+
+    private bool SetPlayerInputData(Player player)
+    {
+        //data from input
+        if (this.playerNick != null)
+            player.ChangeNick(this.playerNick);
+
+        if (this.chips != null)
+            player.TokensCount = Convert.ToInt32(this.chips);
+
+        if (this.xp != null)
+            player.XP = Convert.ToInt32(this.xp);
+
+        return true;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.playerNick = null;
+        this.chips = null;
+        this.xp = null;
     }
     
     // Update is called once per frame
--- UnityProject/PokerGame/Assets/Scripts/UserInterface/CreateTableMenu.cs
+++ UnityProject/PokerGame/Assets/Scripts/UserInterface/CreateTableMenu.cs
@@ -6,10 +6,17 @@ using UnityEngine.SceneManagement;
 
 using PokerGameClasses;
 
+using System;
+
 public class CreateTableMenu : MonoBehaviour
 {
     [SerializeField] private Button createButton;
     [SerializeField] private Button backToMenuButton;
+
+    private string tableName;
+    private string chips;
+    private string xp;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -30,6 +37,7 @@ public class CreateTableMenu : MonoBehaviour
             return;
         }
         GameTable gameTable = p.CreateYourTable("Test Table from HumanPlayer", null);
+        this.SetGameTableInputData(gameTable);
         MyGameManager.Instance.AddTableToGame(gameTable);
         Debug.Log("Player "+p.Nick+ " created table "+gameTable);
         //SceneManager.LoadScene("Table");
@@ -38,5 +46,38 @@ public class CreateTableMenu : MonoBehaviour
     public void OnBackToMenuButton()
     {
         SceneManager.LoadScene("PlayMenu");
+    }
+
+    public void ReadGameTableName(string name)
+    {
+        this.tableName = name;
+        Debug.Log(this.tableName);
+    }
+
+    public void ReadChips(string chips)
+    {
+        this.chips = chips;
+        Debug.Log(this.chips);
+    }
+
+    public void ReadXP(string xp)
+    {
+        this.xp = xp;
+        Debug.Log(this.xp);
+    }
+
+    private bool SetGameTableInputData(GameTable gameTable)
+    {
+        //data from input
+        if (this.tableName != null)
+            gameTable.ChangeName(this.tableName);
+
+        if (this.chips != null)
+            gameTable.Settings.changeMinTokens(Convert.ToInt32(this.chips));
+
+        if (this.xp != null)
+            gameTable.Settings.changeMinXP(Convert.ToInt32(this.xp));
+
+        return true;
     }
 }
