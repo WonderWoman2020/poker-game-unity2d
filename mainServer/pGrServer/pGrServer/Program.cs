0cd388d42d1280c7afbdc1780e475e5f8e11ac8e
WonderWoman <74731481+WonderWoman2020@users.noreply.github.com>
2023-03-10 21:45:55 +0100

    Tested gameplay controller in version from prototype version (+changed git history to remove accidentally added build files)

---
+++ mainServer/pGrServer/pGrServer/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using PokerGameClasses;
+
+namespace pGrServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Card card = new Card(CardSign.Heart, CardValue.Ace);
+            Console.WriteLine(card.Name);
+
+            Player player1 = new HumanPlayer("First player", PlayerType.Human);
+            GameTable gameTable = new GameTable("Table#1", (HumanPlayer)player1);
+            Player player2 = new HumanPlayer("Second player", PlayerType.Human);
+            Player player3 = new HumanPlayer("Third player", PlayerType.Human);
+
+            gameTable.AddPlayer(player2);
+            gameTable.AddPlayer(player3);
+
+            Console.WriteLine(gameTable);
+
+            GameplayController controller = new GameplayController(gameTable);
+
+            controller.dealCards();
+            controller.playTheGame();
+            controller.ConcludeGame();
+        }
+    }
+}
