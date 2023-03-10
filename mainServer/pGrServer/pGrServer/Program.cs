4ad3fad4b3b702c0be84847a8707fd05e4f7a554
WonderWoman <74731481+WonderWoman2020@users.noreply.github.com>
2023-03-10 22:13:06 +0100

    Added game logic from Unity to create server side (by Łukasz) and removed accidentally added build files from repo

---
+++ mainServer/pGrServer/pGrServer/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using PokerGameClasses;
+
+namespace pGrServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Card card = new Card(CardSign.Heart, CardValue.Ace);
+            Console.WriteLine(card.Name);
+        }
+    }
+}
