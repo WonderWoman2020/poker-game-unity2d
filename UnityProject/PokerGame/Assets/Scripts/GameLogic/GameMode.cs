ad092a51ce23e15b7b07c4c63542a4339e0a1d5a
WonderWoman <74731481+WonderWoman2020@users.noreply.github.com>
2023-04-24 23:23:25 +0200

    Deleted some old logic classes

---
+++ UnityProject/PokerGame/Assets/Scripts/GameLogic/GameMode.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokerGameClasses
+{
+    public enum GameMode
+    {
+        No_Bots,
+        You_And_Bots,
+        Mixed
+    }
+
+}
