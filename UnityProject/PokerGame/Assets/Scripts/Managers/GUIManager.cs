e1c0af75c4281b0609c472f34b66406f78d46e43
WonderWoman <74731481+WonderWoman2020@users.noreply.github.com>
2023-04-25 19:45:22 +0200

    Added comments in other Managers classes

--- UnityProject/PokerGame/Assets/Scripts/Managers/GUIManager.cs
+++ UnityProject/PokerGame/Assets/Scripts/Managers/GUIManager.cs
@@ -2,29 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// Klasa, która ma dodawaæ nowe zachowania dla wszystkich ekranów GUI
 public class GUIManager : MonoBehaviour
 {
+    // Dzia³a wed³ug wzorca singleton
     public static GUIManager Instance
     { get; set; }
 
     void Awake()
     {
+        // Wzorzec singleton (ma zawsze istnieæ tylko 1 instancja tej klasy)
         if (Instance != null && Instance != this)
             Destroy(this);
         else
             Instance = this;
-
-        //DontDestroyOnLoad(Instance.gameObject);
     }
     // Update is called once per frame
     void Update()
     {
+        // Jeœli w którymœ ekranie zostanie naciœniêty 'Esc', wy³¹czy aplikacjê
         if (Input.GetKeyDown(KeyCode.Escape))
         {
+            // ¯eby dzia³a³o te¿ w edytorze Unity (dla dewelopera)
             #if UNITY_EDITOR
                     UnityEditor.EditorApplication.isPlaying = false;
             #endif
 
+            // ¯eby dzia³a³o w zbudowanej wersji gry
             Application.Quit();
         }
     }
--- UnityProject/PokerGame/Assets/Scripts/Managers/PersistentManagers.cs
+++ UnityProject/PokerGame/Assets/Scripts/Managers/PersistentManagers.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// Klasa, która zapewnia nie niszczenie GameObject'u,
+// do którego zosta³a do³¹czona
 public class PersistentManagers : MonoBehaviour
 {
     void Awake()
