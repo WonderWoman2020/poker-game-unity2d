208fa3c2ce64962663b3f91cc0602cfd2bd93e48
WonderWoman <74731481+WonderWoman2020@users.noreply.github.com>
2023-06-24 20:18:47 +0200

    Added ChangePasswordMenu scene to be reachable in game (+ connected inpu fields)

---
+++ UnityProject/PokerGame/Assets/Scripts/UserInterface/Scenes/Menu/ChangePasswordMenu.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using TMPro;
+using UnityEngine.SceneManagement;
+
+//using System.Net.Sockets;
+
+//using PokerGameClasses;
+//using pGrServer;
+
+
+
+public class ChangePasswordMenu : MonoBehaviour
+{
+
+    [SerializeField] private Button changePasswordButton;
+    [SerializeField] private Button backButton;
+
+    public GameObject PopupWindow;
+
+    private string currentPassword;
+    private string newPassword;
+    private string confirmPassword;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnChangePasswordButton()
+    {
+        Debug.Log("Change");
+    }
+
+    public void OnBackButton()
+    {
+        SceneManager.LoadScene("SettingsMenu");
+    }
+
+    public void ReadCurrentPassword(string currentPswd)
+    {
+        if (currentPswd.Length == 0)
+        {
+            this.currentPassword = null;
+            return;
+        }
+
+        this.currentPassword = currentPswd;
+        Debug.Log(this.currentPassword);
+    }
+
+    public void ReadNewPassword(string newPswd)
+    {
+        if (newPswd.Length == 0)
+        {
+            this.newPassword = null;
+            return;
+        }
+
+        this.newPassword = newPswd;
+        Debug.Log(this.newPassword);
+    }
+
+    public void ReadConfirmPassword(string confirmPswd)
+    {
+        if (confirmPswd.Length == 0)
+        {
+            this.confirmPassword = null;
+            return;
+        }
+
+        this.confirmPassword = confirmPswd;
+        Debug.Log(this.confirmPassword);
+    }
+
+    void ShowPopup(string text)
+    {
+        var popup = Instantiate(PopupWindow, transform.position, Quaternion.identity, transform);
+        popup.GetComponent<TextMeshProUGUI>().text = text;
+    }
+}
--- UnityProject/PokerGame/Assets/Scripts/UserInterface/Scenes/Menu/SettingsMenu.cs
+++ UnityProject/PokerGame/Assets/Scripts/UserInterface/Scenes/Menu/SettingsMenu.cs
@@ -35,7 +35,7 @@ public class SettingsMenu : MonoBehaviour
 
     public void OnChangePasswordButton()
     {
-
+        SceneManager.LoadScene("ChangePasswordMenu");
     }
 
     public void OnChangeNameButton()
