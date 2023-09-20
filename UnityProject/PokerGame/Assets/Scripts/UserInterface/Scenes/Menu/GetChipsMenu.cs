f6d30a0a8ec6f1125581af2768ac064c2f503261
KatarzynaLam <kasialamczyk@wp.pl>
2023-09-20 21:33:14 +0200

    Fix GetToken issue

--- UnityProject/PokerGame/Assets/Scripts/UserInterface/Scenes/Menu/GetChipsMenu.cs
+++ UnityProject/PokerGame/Assets/Scripts/UserInterface/Scenes/Menu/GetChipsMenu.cs
@@ -38,6 +38,7 @@ public class GetChipsMenu : MonoBehaviour
         byte[] toSend = System.Text.Encoding.ASCII.GetBytes(token + ' ' + "7" + ' ');
         mainServer.stream.Write(toSend, 0, toSend.Length);
         mainServer.stream.Flush();
+        Thread.Sleep(100);
         if (mainServer.stream.DataAvailable)
         {
             byte[] readBuf = new byte[4096];
@@ -47,22 +48,32 @@ public class GetChipsMenu : MonoBehaviour
             menuRequestStr.AppendFormat("{0}", Encoding.ASCII.GetString(readBuf, 0, nrbyt));
             string[] response = menuRequestStr.ToString().Split(new string(":T:"));
             string[] splitResponse = response[0].Split(' ');
+
+            splitResponse[2] = "0";
+
+            Debug.Log("Chips response: " + response[0]);
+            Debug.Log("Chips splitResponse: " + splitResponse[2]);
+
             if (response[0] == "answer Z 1 ")
             {
+                Debug.Log("IF ERROR");
                 ShowPopup("Error: bad request");
             }
             else if (splitResponse[2] == "1")
             {
+                Debug.Log("IF 1");
                 ShowPopup("You can't receive more chips now! You'll be able to collect more in " + splitResponse[3] + " hours");
                 return;
             }
             else if (splitResponse[2] == "2")
             {
+                Debug.Log("IF 2");
                 ShowPopup("You can't get more tokens while sitting at a table!");
                 return;
             }
-            else if (splitResponse[0] == "0")
+            else if (splitResponse[2] == "0")
             {
+                Debug.Log("IF 0");
                 ShowPopup("Received " + splitResponse[3] + " chips!");
                 int coins = Convert.ToInt32(splitResponse[3]);
                 MyGameManager.Instance.MainPlayer.TokensCount += coins;
@@ -70,10 +81,15 @@ public class GetChipsMenu : MonoBehaviour
             }
             else if (response[0] == "answer 7 A ")
             {
+                Debug.Log("IF BAD");
                 ShowPopup("Something went wrong with sending information to the server, please try again later");
                 return;
             }
+            Debug.Log("Chips response: " + response[0]);
+            Debug.Log("Chips splitResponse: " + splitResponse[2]);
+            Debug.Log("KONIEC1");
         }
+        Debug.Log("KONIEC2");
     }
 
     public void onBackToMenuButton ()
