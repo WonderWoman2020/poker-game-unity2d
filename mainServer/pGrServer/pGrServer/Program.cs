cf1a46c7ad3b27c43ae8a80657394752a4f3b473
lukasz4545 <lukasz.szurek@proton.me>
2023-12-21 22:28:02 +0100

    seatNr

--- mainServer/pGrServer/pGrServer/Program.cs
+++ mainServer/pGrServer/pGrServer/Program.cs
@@ -248,7 +248,8 @@ namespace pGrServer
                                                 {
                                                     if(ppl != client)
                                                     {
-                                                        byte[] info = System.Text.Encoding.ASCII.GetBytes("answer add " + ppl.Nick + " " + ppl.TokensCount + " " + ppl.XP + " ");
+                                                        byte[] info = System.Text.Encoding.ASCII.GetBytes("answer add " + ppl.Nick + " " + ppl.TokensCount + 
+                                                            " " + ppl.XP + " " + ppl.SeatNr + " ");
                                                         ppl.MenuRequestsStream.Write(info, 0, info.Length);
                                                     }
                                                 }
