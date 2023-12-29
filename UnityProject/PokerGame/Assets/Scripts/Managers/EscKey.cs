aee22646840f4ab9c0d41afc8096780e44f54159
KatarzynaLam <kasialamczyk@wp.pl>
2023-12-29 19:32:24 +0100

    Added ESC button in table and in scenes

---
+++ UnityProject/PokerGame/Assets/Scripts/Managers/EscKey.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EscKey : MonoBehaviour
+{
+
+    [SerializeField]
+    private string scene;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene(scene);
+        }
+    }
+
+
+}
